f3629dc61d24b83cb9e7ea2a5a746fb4b9c51ae7
Comrade-Eggoz <nishant.mahajan@eggoz.in>
2025-08-15 18:05:29 +0530

    On login now we are able to redirect to home dashboard

--- flow/settings.py
+++ flow/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'flow.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'frontend'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- flow/urls.py
+++ flow/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from .views import home_dashboard
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('dashboard/', home_dashboard, name='dashboard'),
     path('song/',include('song.urls')),
     path('user/',include('user.urls')),
 ]
